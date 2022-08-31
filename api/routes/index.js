4f9a8122647a32687dfff68e4e8e054ec73454c6
Sami Haddad <77640501+samihaddad99@users.noreply.github.com>
2022-08-31 15:26:09 -0400

    updated CORS policy

--- api/routes/index.js
+++ api/routes/index.js
@@ -10,7 +10,7 @@ const merchant_model = require('../merchant_model') // import the merchant funct
 
 app.use(express.json());
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
