e220f159fb8b77dd204ee85f7a7cac2305f95774
Sami Haddad <77640501+samihaddad99@users.noreply.github.com>
2022-09-01 20:23:37 -0400

    changed the connection string

--- api/merchant_model.js
+++ api/merchant_model.js
@@ -14,14 +14,24 @@ const { Pool } = require('pg')
 //   ssl: isProduction ? { rejectUnauthorized: false } : false,
 // });
 
-// local pool
+// heroku pool
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 const pool = new Pool({
-  user: 'nglyfwwl',
-  host: 'heffalump.db.elephantsql.com',
-  database: 'nglyfwwl',
-  password: 'VgEsZXQmoxonCtdAw9xXgMNEJdeeLBQt',
-  port: 5432,
+  connectionString: isProduction
+    ? process.env.DATABASE_URL // Heroku will supply us with a string called DATABASE_URL for the connectionString,
+    : connectionString,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
+
+// local pool
+// const pool = new Pool({
+//   user: 'nglyfwwl',
+//   host: 'heffalump.db.elephantsql.com',
+//   database: 'nglyfwwl',
+//   password: 'VgEsZXQmoxonCtdAw9xXgMNEJdeeLBQt',
+//   port: 5432,
+// });
 
 const getMerchants = () => {
   return new Promise(function(resolve, reject) {
