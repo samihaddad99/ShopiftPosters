53ec6904824d320edf830c14d7a7612bf0008f45
Sami Haddad <77640501+samihaddad99@users.noreply.github.com>
2022-09-01 23:37:51 -0400

    deleted connection

--- api/merchant_model.js
+++ api/merchant_model.js
@@ -17,7 +17,6 @@ const { Pool } = require('pg')
 // heroku pool
 const isProduction = process.env.NODE_ENV === "production";
 const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-const connection = DATABASE_URL;
 const pool = new Pool({
   connectionString: isProduction
     ? process.env.DATABASE_URL // Heroku will supply us with a string called DATABASE_URL for the connectionString,
