9442eae62a969d5542e7391be443b63ec1de321b
Sami Haddad <77640501+samihaddad99@users.noreply.github.com>
2022-09-01 23:04:40 -0400

    testing get call

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,7 +5,7 @@
 const express = require('express');
 var router = express.Router();
 const app = express()
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001; // used to be just 3001
 const merchant_model = require('../merchant_model') // import the merchant functions
 const cors = require("cors");
 const morgan = require("morgan");
@@ -14,24 +14,25 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+//   next();
+// });
 
 // changed all 'app' to 'router'
 app
 .route('/')
 .get((req, res) => {
-  merchant_model.getMerchants()
-  .then(response => {
-    res.status(200).send(response);
-  })
-  .catch(error => {
-    res.status(500).send(error);
-  })
+  // merchant_model.getMerchants()
+  // .then(response => {
+  //   res.status(200).send(response);
+  // })
+  // .catch(error => {
+  //   res.status(500).send(error);
+  // })
+  console.log("get / call");
 })
 
 app
